a7d53fee28f145142584ea848e619668209ab32e
Jon-Ajumobi Olamide D <Olamideajumobi@gmail.com>
2021-04-07 15:12:35 +0100

    Added Grpc Server and Client.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServerDemo;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var greeterClient = new Greeter.GreeterClient(channel);
+            var customerClient = new Customers.CustomersClient(channel);
+            var reply= await greeterClient.SayHelloAsync(new HelloRequest { Name = "Jon-Ajumobi Olamide D." });
+            Console.WriteLine($"Message is: {reply.Message}");
+
+            using (var customer = customerClient.GetCustomers(new EmptyModel()))
+            {
+                while(await customer.ResponseStream.MoveNext())
+                {
+                    var current = customer.ResponseStream.Current;
+                    Console.WriteLine($"{++x}. Customer Name: {current.Name}, Customer Email: {current.Email}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServerDemo/Services/CustomersService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcServerDemo.Services
+{
+    public class CustomersService : Customers.CustomersBase
+    {
+        public CustomersService()
+        {
+
+        }
+        public override async Task GetCustomers(EmptyModel request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            var customers = new List<CustomerModel> {
+             new CustomerModel{Email= "Ike.ajumobi@mail.com", Name="Ike"},
+             new CustomerModel{Email= "Ola.ajumobi@mail.com", Name="Ola"},
+             new CustomerModel{Email= "Ife.ajumobi@mail.com", Name="Ife"},
+            };
+            foreach(var customer in customers)
+            {
+                await responseStream.WriteAsync(customer);
+            }
+        }
+    }
+}
