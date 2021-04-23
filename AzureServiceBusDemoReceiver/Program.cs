7d587c8016ff2507ce47af647ede1c483b3bd7b8
Jon-Ajumobi Olamide D <Olamideajumobi@gmail.com>
2021-04-23 20:50:08 +0100

    Worked on Configuring Depedency Injection and Application Configurations

--- AzureServiceBusDemoReceiver/Program.cs
+++ AzureServiceBusDemoReceiver/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AzureServiceBusModels;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -10,20 +12,16 @@ using System.Threading.Tasks;
 
 namespace AzureServiceBusDemoReceiver
 {
-    class Program
+    public class Main
     {
-        private static IQueueClient queueClient;
-        private static ISubscriptionClient subscriptionClient;
-        public static async Task Main(string[] args)
+        private readonly ISubscriptionClient subscriptionClient;
+
+        public Main(ISubscriptionClient subscriptionClient)
         {
-            Console.WriteLine("Hello, Welcome to my Azure Service Bus Receiver");
-            var configuration = GetConfiguration();
-            string connectionstring = configuration["AzureServiceBus:ConnectionString"];
-            string queueName = configuration["AzureServiceBus:Queue"];
-            string topicName = configuration["AzureServiceBus:Topic"];
-            string subscriptionName = configuration["AzureServiceBus:Subscription"];
-            //queueClient = new QueueClient(connectionstring, queueName);
-            subscriptionClient = new SubscriptionClient(connectionstring, topicName, subscriptionName);
+            this.subscriptionClient = subscriptionClient;
+        }
+        public async Task Run()
+        {
             var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
             {
                 MaxConcurrentCalls = 1,
@@ -34,28 +32,53 @@ namespace AzureServiceBusDemoReceiver
             //await queueClient.CloseAsync();
             await subscriptionClient.CloseAsync();
         }
-
-        private static async Task ProcessMessageAsync(Message messsage, CancellationToken cancellationToken)
-        {
-            string body = Encoding.UTF8.GetString(messsage.Body);
-            var person = JsonConvert.DeserializeObject<Person>(body);
-            Console.WriteLine($"Person Received, Name: {person.Name}, Age: {person.Age}");
-            await subscriptionClient.CompleteAsync(messsage.SystemProperties.LockToken);
-        }
-
         private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
         {
             Console.WriteLine($"Something has occurred, {arg.Exception}");
             return Task.CompletedTask;
         }
 
-        public static IConfiguration GetConfiguration()
+        private async Task ProcessMessageAsync(Message messsage, CancellationToken cancellationToken)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+            string body = Encoding.UTF8.GetString(messsage.Body);
+            var person = JsonConvert.DeserializeObject<Person>(body);
+            Console.WriteLine($"Person Received, Name: {person.Name}, Age: {person.Age}");
+            await subscriptionClient.CompleteAsync(messsage.SystemProperties.LockToken);
+        }
+    }
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureHostConfiguration(configBuilder=> SetUpConfiguration(configBuilder)).ConfigureServices(serviceCollection => {
+                ConfigureServices(serviceCollection);
+            });
+        public static void SetUpConfiguration(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddUserSecrets<Program>();
-            return builder.Build();
+        }
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<Main>();
+            services.AddTransient<ISubscriptionClient>(sp =>
+            {
+                IConfiguration configuration = sp.GetService<IConfiguration>();
+                string connectionstring = configuration["AzureServiceBus:ConnectionString"];
+                string queueName = configuration["AzureServiceBus:Queue"];
+                string topicName = configuration["AzureServiceBus:Topic"];
+                string subscriptionName = configuration["AzureServiceBus:Subscription"];
+                //queueClient = new QueueClient(connectionstring, queueName);
+                return new SubscriptionClient(connectionstring, topicName, subscriptionName);
+            });
+        }
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to my Azure Service Bus Receiver");
+            var host= CreateHostBuilder(args: args).Build();
+            var mainService=host.Services.GetService<Main>();
+            await mainService.Run();
+            Console.WriteLine("Thanks, Bye!");
         }
     }
 }
